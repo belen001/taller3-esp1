cfa90e26c365f1a7bf57a9a73d0e16d239e07a63
CQuarkH <currihuile@gmail.com>
2024-05-25 19:18:30 -0400

    Markdown support and TailwindCSS Typography added. md directory created to store md files.

--- taller3-esp1/src/views/Home.vue
+++ taller3-esp1/src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup>
-// wiki component
+import Wiki from '../components/md/Wiki.md';
+
 </script>
 <template>
-    <main class="flex flex-col gap-4">
-
-
+    <main class="flex flex-col gap-4 p-6">
+        <article class="prose lg:prose-xl">
+            <Wiki />
+        </article>
     </main>
 </template>
